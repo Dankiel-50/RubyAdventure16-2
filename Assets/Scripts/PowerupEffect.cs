c2c4ca44739b986dd3dcfd32193161ce9c665314
Dankiel-50 <dlkrupczak@gmail.com>
2023-12-06 21:50:39 -0500

    kill

--- Assets/Scripts/PowerupEffect.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//PowerupEffects code by Ryan Horton
-public abstract class PowerupEffect : ScriptableObject
-{
-   public abstract void Apply(GameObject target);
-}
